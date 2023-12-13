90b5423a82244f42a9db4f11b036a92352d0f77a
Matt Smida <mcs@m4t7.com>
2023-12-12 20:44:01 -0500

    added a few comments from code review

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "peanut"
 
 connect_db(app)
-db.create_all()
+db.create_all()   # This doesn't need to run every time! Could go in a seed.py
+                  # and/or just run this in ipython once.
 
 toolbar = DebugToolbarExtension(app)
 bcrypt = Bcrypt()
@@ -31,6 +32,10 @@ def show_homepage():
 def register_user():
     """showing and procesing form to register user"""
 
+    # TODO:
+    # Check to see whether logged in, and if so, send to user page.
+    # (See if username is in the session, which is not just a flask/py thing.)
+
     form = CreateUserForm()
 
     if form.validate_on_submit():
@@ -40,13 +45,16 @@ def register_user():
         last_name = form.last_name.data
         password_hash = bcrypt.generate_password_hash(
             form.password.data).decode('utf8')
+        # Have a class method on the model to register the user
+        # e.g., username.register(), defined in model
+        # ...which returns an instance of the user.
 
         user = User(
             username=username,
             email=email,
-            first_name=first_name,
-            last_name=last_name,
-            password=password_hash)
+            first_name=first_name,    # See Tuesday notes for an example
+            last_name=last_name,      # (class method in User for this:)
+            password=password_hash)   # All of this should also be done in m.py
 
         session["username"] = username
 
@@ -62,6 +70,7 @@ def register_user():
 @app.route('/login', methods=['GET', 'POST'])
 def log_in_user():
     """ showing and processing login form"""
+    # Needs to have a redirect to the user info page if logged in. TODO:
 
     form = LoginUserForm()
 
@@ -69,7 +78,7 @@ def log_in_user():
         username = form.username.data
         password = form.password.data
 
-        user = User.authenticate(username, password)
+        user = User.authenticate(username, password)  # <-- This is good! More.
 
         if user:
             session["username"] = user.username
@@ -79,6 +88,7 @@ def log_in_user():
             flash('username or password is incorrect')
             return render_template('login.html', form=form)
 
+    # Will only run if v_o_s() returns False, so get rid of the *else*.
     else:
         return render_template('login.html', form=form)
 
@@ -97,9 +107,16 @@ def show_user_info(username):
         "last_name": user.last_name
     }
 
+    # Just pass user over, then in the template, do user.email, user.fname etc
+    # ...But is it a security risk to pass user over to the template?
+    # No, it's not because user.password will give the hashed password
+    # which is already hard to get into because you'd need to get into the
+    # backend.
+
     return render_template('user_info.html', form=form, user_info=user_info)
 
 
+# Logout should be near login here in the code. Why? Logical grouping of rtes.
 @app.post('/logout')
 def logout():
     """ Log out the current user and redirect to the root route. """
